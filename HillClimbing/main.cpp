4b54406c38166858be9ebc46ee1acc7e54e3d5fc
Yakup Ates <Yakup.Ates@rub.de>
2017-09-28 13:30:01 +0200

    Revert "not working yet, need const double* from struct"
    This reverts commit edcd20aa756d7a8088dc58ad2d25aaea2aab4f40.

--- HillClimbing/main.cpp
+++ HillClimbing/main.cpp
@@ -9,43 +9,23 @@
 
 #define VALUE_COUNT 1000
 
-struct coordinate {
-	double x;
-	double y;
-};
-
-coordinate f(int x) {
-	coordinate point;
-	
-    //double x[VALUE_COUNT];
-    //double y[VALUE_COUNT];
-
-    //for (int i = 0; i < VALUE_COUNT; ++i) {
-	point.x = x / 10.0;
-	point.y = sin(point.x);
-        //x[i] =  i / 10.0;
-        //y[i] = sin(x[i]);
-		//}
-
-	return point;
-}
 
 int main(int argc, char **argv)
 {
     QApplication a(argc, argv);
+    double x[VALUE_COUNT];
+    double y[VALUE_COUNT];
 
-	//double x[VALUE_COUNT];
-    //double y[VALUE_COUNT];
-	coordinate coords[VALUE_COUNT];
     for (int i = 0; i < VALUE_COUNT; ++i) {
-		coords[i] = f(i);
-	}
-	
+        x[i] =  i / 10.0;
+        y[i] = sin(x[i]);
+    }
+
     QwtPlot plot;
     QwtPlotCurve *curve = new QwtPlotCurve();
 
 	// insert data
-    curve->setRawData((*coords).x, (*coords).y, VALUE_COUNT);
+    curve->setRawData(x, y, VALUE_COUNT);
     curve->attach(&plot);
 	
 	// configure plot
@@ -53,7 +33,8 @@ int main(int argc, char **argv)
 	plot.setAxisTitle(QwtPlot::xBottom, "x");
 	plot.setTitle(PLOT_TITLE);
 	plot.axisScaleEngine(QwtPlot::xBottom)->setAttribute(QwtScaleEngine::Floating,true);
-	// plot.setAxisScale(QwtPlot::xBottom, 0, VALUE_COUNT/10, 1);
+	plot.setAxisScale(QwtPlot::xBottom, 0, VALUE_COUNT/10, 1);
+	plot.resize(200, 200);
 
 	// show plot
     plot.show();
